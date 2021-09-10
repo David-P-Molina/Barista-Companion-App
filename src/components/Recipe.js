1aa095e16248fbd514e352bc0984b725dbe0ea32
David Moina <davemolina777@gmail.com>
2021-09-10 11:52:29 -0500

    Add basic recipe component structure

---
+++ src/components/Recipe.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Recipe extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Recipe Name</h1>
+                <ul>
+                    <li>Ingredients</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Recipe
