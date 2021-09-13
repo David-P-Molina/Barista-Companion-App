292fc9af1bfed43b834eaa6fc23a99385b39ad5c
David Moina <davemolina777@gmail.com>
2021-09-13 14:05:14 -0500

    Add basic layout for Layout

--- src/components/CoffeeBean.js
+++ src/components/CoffeeBean.js
@@ -4,7 +4,10 @@ class CoffeeBean extends Component {
     render() {
         return (
             <div>
-                
+                <h1>
+                {this.props.name} - {this.props.roasters.name}
+                </h1>
+                {this.props.description}
             </div>
         )
     }
