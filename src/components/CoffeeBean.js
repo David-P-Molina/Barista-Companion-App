4d26837b94352cb081ca2d461fe8a5012d609559
David Moina <davemolina777@gmail.com>
2021-09-13 14:03:24 -0500

    Create Basic CoffeeBean Component

---
+++ src/components/CoffeeBean.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class CoffeeBean extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default CoffeeBean
