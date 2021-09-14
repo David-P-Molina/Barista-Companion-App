aca831c138040148a0d42d7e22602abdb576f16b
David Moina <davemolina777@gmail.com>
2021-09-14 17:50:00 -0500

    Create handleOnChange method

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -10,16 +10,22 @@ class RecipeForm extends Component {
         grind: '',
         time: '',
     }
+    handleOnChange = (e) => {
+        this.setState({
+        [e.target.name]: e.target.value
+        })
+    }
+
     render() {
         return (
             <div>
-                <input type="text" value={this.state.name} onChange={}/>
-                <input type="text" value={this.state.brewMethod} onChange={}/>
-                <input type="number" value={this.state.temperature} onChange={}/>
-                <input type="number" value={this.state.waterWeight} onChange={}/>
-                <input type="text" value={this.state.coffeeBean} onChange={}/>
-                <input type="text" value={this.state.grind} onChange={}/>
-                <input type="time" value={this.state.time} onChange={}/>
+                <input type="text" value={this.state.name} onChange={this.handleOnChange}/>
+                <input type="text" value={this.state.brewMethod} onChange={this.handleOnChange}/>
+                <input type="number" value={this.state.temperature} onChange={this.handleOnChange}/>
+                <input type="number" value={this.state.waterWeight} onChange={this.handleOnChange}/>
+                <input type="text" value={this.state.coffeeBean} onChange={this.handleOnChange}/>
+                <input type="text" value={this.state.grind} onChange={this.handleOnChange}/>
+                <input type="time" value={this.state.time} onChange={this.handleOnChange}/>
             </input>
         )
     }
