adfd34bc54933f22d7bd8c4d5f40112c28d3d4dd
David Moina <davemolina777@gmail.com>
2021-09-14 17:47:11 -0500

    Create basic imputs

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -13,8 +13,14 @@ class RecipeForm extends Component {
     render() {
         return (
             <div>
-                
-            </div>
+                <input type="text" value={this.state.name} onChange={}/>
+                <input type="text" value={this.state.brewMethod} onChange={}/>
+                <input type="number" value={this.state.temperature} onChange={}/>
+                <input type="number" value={this.state.waterWeight} onChange={}/>
+                <input type="text" value={this.state.coffeeBean} onChange={}/>
+                <input type="text" value={this.state.grind} onChange={}/>
+                <input type="time" value={this.state.time} onChange={}/>
+            </input>
         )
     }
 }
