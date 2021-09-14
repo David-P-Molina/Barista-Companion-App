3527d2956d975d9eaf43e25e45311ed5ce071572
David Moina <davemolina777@gmail.com>
2021-09-14 17:43:21 -0500

    set initial state

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
 class RecipeForm extends Component {
+    state = {
+        name: '',
+        brewMethod: '',
+        temperature: '',
+        waterWeight: '',
+        coffeeBean: '',
+        grind: '',
+        time: '',
+    }
     render() {
         return (
             <div>
