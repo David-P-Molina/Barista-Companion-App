7e46624171dafeb726f06b59edeca8464418f87a
David Moina <davemolina777@gmail.com>
2021-09-15 09:20:14 -0500

    Create brewmethod form and basic component layout

---
+++ src/components/BrewMethodForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BrewMethodForm extends Component {
+    render() {
+        return (
+            <form>
+                
+            </form>
+        )
+    }
+}
+
+export default BrewMethodForm
