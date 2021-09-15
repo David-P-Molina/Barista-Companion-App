cec1752790d3e58e35e705a32eca952a4ebfbbc8
David Moina <davemolina777@gmail.com>
2021-09-15 09:11:29 -0500

    Add breaks tags and submit button

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -24,20 +24,21 @@ class RecipeForm extends Component {
         return (
             <div>
                 <form className="recipe-form" onSubmit={this.handleOnSubmit}>
-                    <label htmlFor="recipe-name">Recipe Name: </label>
-                    <input type="text" value={this.state.name} onChange={this.handleOnChange}/>
-                    <label htmlFor="brew-method">Brew Method: </label>
-                    <input type="text" value={this.state.brewMethod} onChange={this.handleOnChange}/>
-                    <label htmlFor="temperature">Temperature: </label>
-                    <input type="number" value={this.state.temperature} onChange={this.handleOnChange}/>
-                    <label htmlFor="water-weight">Water Weight: </label>
-                    <input type="number" value={this.state.waterWeight} onChange={this.handleOnChange}/>
-                    <label htmlFor="coffee-bean">Coffee Bean: </label>
-                    <input type="text" value={this.state.coffeeBean} onChange={this.handleOnChange}/>
-                    <label htmlFor="grind">Grind Size: </label>
-                    <input type="text" value={this.state.grind} onChange={this.handleOnChange}/>
-                    <label htmlFor="time">Brew Time: </label>
-                    <input type="time" value={this.state.time} onChange={this.handleOnChange}/>
+                    <label htmlFor="recipe-name">Recipe Name: </label> <br />
+                    <input type="text" value={this.state.name} onChange={this.handleOnChange}/> <br />
+                    <label htmlFor="brew-method">Brew Method: </label> <br />
+                    <input type="text" value={this.state.brewMethod} onChange={this.handleOnChange}/> <br />
+                    <label htmlFor="temperature">Temperature: </label> <br />
+                    <input type="number" value={this.state.temperature} onChange={this.handleOnChange}/> <br />
+                    <label htmlFor="water-weight">Water Weight: </label> <br />
+                    <input type="number" value={this.state.waterWeight} onChange={this.handleOnChange}/> <br />
+                    <label htmlFor="coffee-bean">Coffee Bean: </label> <br />
+                    <input type="text" value={this.state.coffeeBean} onChange={this.handleOnChange}/> <br />
+                    <label htmlFor="grind">Grind Size: </label> <br />
+                    <input type="text" value={this.state.grind} onChange={this.handleOnChange}/> <br />
+                    <label htmlFor="time">Brew Time: </label> <br />
+                    <input type="time" value={this.state.time} onChange={this.handleOnChange}/> <br />
+                    <button type="submit">Create Recipe</button>
                 </form>
             </div>
         )
