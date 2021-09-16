92fc9f6897f4952a445870a6ca5fab0686f5e851
David Moina <davemolina777@gmail.com>
2021-09-16 11:11:50 -0500

    Add basic brew method form

--- src/components/BrewMethodForm.js
+++ src/components/BrewMethodForm.js
@@ -22,8 +22,14 @@ class BrewMethodForm extends Component {
     }
     render() {
         return (
-            <form>
-                
+            <form className='brew-method-form' onSubmit={this.handleOnSubmit}>
+                <label htmlFor='brew-method-name'>Brew Method: </label>
+                <input type='text' value={this.state.name} onChange={this.handleOnChange} />
+                <label htmlFor='brew-method-description'>Description: </label>
+                <input type='text' value={this.state.description} onChange={this.handleOnChange} />
+                <label htmlFor='brew-method-equipment'>Equipment: </label>
+                <input type='text' value={this.state.equipment} onChange={this.handleOnChange} />
+                <input type='submit'> Create Brew Method </input>
             </form>
         )
     }
