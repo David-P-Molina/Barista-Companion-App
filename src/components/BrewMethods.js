296a45ec7096d1b86eb247d67ad76deb1b86828f
David Moina <davemolina777@gmail.com>
2021-09-17 12:52:23 -0500

    Add basic mapOverBrewMethods

--- src/components/BrewMethods.js
+++ src/components/BrewMethods.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 
 export class BrewMethods extends Component {
-
+    mapOverBrewMethods = () => {
+        let brewMethods = {this.props.brewMethods}
+        brewMethods.map((brew) => {
+            
+        })
+    }
     render() {
         return (
             <div>
