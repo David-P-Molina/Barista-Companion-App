aaa61efb4e737ad931c5bb2debea55607e0f6192
David Moina <davemolina777@gmail.com>
2021-09-17 13:21:30 -0500

    Add basic state and onchange to inputs

--- src/components/CoffeeBeanForm.js
+++ src/components/CoffeeBeanForm.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 
 class CoffeeBeanForm extends Component {
+    (:name, :roaster_id, :user_id, :roast, :whole_bean, :description, :notes)
+    state = {
+        name: '',
+        roaster: '',
+        userId: '',
+        roast: '',
+        wholeBean: '',
+        description: '',
+        notes: '',
+    }
     handleOnChange = (e) => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value={this.state.} onChange
         })
     }
     render() {
@@ -11,17 +21,19 @@ class CoffeeBeanForm extends Component {
             <div className='coffee-bean-form'>
                 <form>
                     <label htmlFor='coffee-bean-name'>Name: </label>
-                    <input type='text' value={this.handleOnChange}>
+                    <input type='text' value={this.state.name} onChange={this.handleOnChange} />
                     <label htmlFor='roaster'>Roaster: </label>
-                    <input type='select' value={this.handleOnChange} />
+                    <input type='select' value={this.state.roaster} onChange={this.handleOnChange} />
                     <label htmlFor='roast-level'>Roast Level: </label>
-                    <input type='select' value={this.handleOnChange} />
+                    <input type='select' value={this.state.roast} onChange={this.handleOnChange} />
                     <label htmlFor='whole-bean-boolean'>Whole Bean:</label>
-                    <input type='checkbox' value={this.handleOnChange} />
+                    <input type='checkbox' value={this.state.wholeBean} onChange={this.handleOnChange} />
+                    <label htmlFor='description'>Description: </label>
+                    <input type='text' value={this.state.description} onChange={this.handleOnChange}/>
+                    <label htmlFor='notes'>Notes: </label>
+                    <input type='text' value={this.state.notes} onChange={this.handleOnChange}/>
                     <input type='submit' />
                 </form>
-                (:name, :roaster_id, :user_id, :roast, :whole_bean, :description, :notes)
-    end
             </div>
         )
     }
