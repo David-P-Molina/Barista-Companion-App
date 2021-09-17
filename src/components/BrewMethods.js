6bcf7f1d117e6ab744e02bc133f2062c7c13cb73
David Moina <davemolina777@gmail.com>
2021-09-17 12:51:58 -0500

    Create basic brewMethods component

---
+++ src/components/BrewMethods.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class BrewMethods extends Component {
+
+    render() {
+        return (
+            <div>
+                {this.mapOverBrewMethods}
+            </div>
+        )
+    }
+}
+
+export default BrewMethods
