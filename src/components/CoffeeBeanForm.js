ae9cab34fd64eb95e85c26cae574411483bd679f
David Moina <davemolina777@gmail.com>
2021-09-17 13:09:59 -0500

    Add basic form

---
+++ src/components/CoffeeBeanForm.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class CoffeeBeanForm extends Component {
+    render() {
+        return (
+            <div className='coffee-bean-form'>
+                <form>
+                    <label htmlFor='coffee-bean-name'>Name: </label>
+                    <input type='text' value=''>
+                    <label htmlFor='roaster'>Roaster: </label>
+                    <input type='select' />
+                    <label htmlFor='roast-level'>Roast Level: </label>
+                    <input type='select' />
+                    <label htmlFor='whole-bean-boolean'>Whole Bean:</label>
+                    <input type='checkbox' />
+                    <input type='submit' />
+                </form>
+                (:name, :roaster_id, :user_id, :roast, :whole_bean, :description, :notes)
+    end
+            </div>
+        )
+    }
+}
+
+export default CoffeeBeanForm
