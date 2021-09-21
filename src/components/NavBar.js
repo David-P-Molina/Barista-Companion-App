9c847146f109bde8c8b25d3b668825caf11cfa02
David Moina <davemolina777@gmail.com>
2021-09-21 10:04:13 -0500

    Add basic navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-route-dom'
 
 class NavBar extends Component {
     render() {
         return (
             <div>
-                
+                <NavLink to="/roasters"> Roasters </NavLink>
+                <NavLink to="/coffee-beans"> Coffee Beans </NavLink>
+                <NavLink to="/brew-methods"> Brew Methods </NavLink>
+                <NavLink to="/recipes"> Recipes </NavLink>
             </div>
         )
     }
