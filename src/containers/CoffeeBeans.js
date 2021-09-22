354495c86af0b535385073337e690317e8ef87f0
David Moina <davemolina777@gmail.com>
2021-09-22 17:14:08 -0500

    Create basic component structure

---
+++ src/containers/CoffeeBeans.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class CoffeeBeans extends Component {
+    constructor() {
+        super()
+        this.state = {
+            
+        }
+    }
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CoffeeBeans
