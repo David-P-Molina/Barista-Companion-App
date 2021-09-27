5437fc82974970592dd6569a0a70f783a99608f6
David Moina <davemolina777@gmail.com>
2021-09-27 17:43:45 -0500

    Create basic reducer for brew method and fix typos

---
+++ src/reducers/brewMethodsReducer.js
@@ -0,0 +1,15 @@
+function brewMethodsReducer(state = [], action) {
+    let idx
+    switch (action.type) {
+        case 'ADD_BREW_METHOD':
+            return [...state, action.book]
+            
+        case 'DELETE_BREW_METHOD':
+            idx = state.findIndex((brew) => brew.id === action.id)
+            return [...state.slice(0, idx), ...state.slice(idx + 1)]
+
+        default:
+            return state
+    }
+}
+export default brewMethodsReducer
--- src/reducers/coffeeBeansReducer.js
+++ src/reducers/coffeeBeansReducer.js
@@ -3,10 +3,10 @@ function coffeeBeansReducer(state = [], action) {
     switch (action.type) {
         case 'ADD_COFFEE_BEAN':
             return [...state, action.book]
-            
+
         case 'DELETE_COFFEE_BEAN':
             idx = state.findIndex((bean) => bean.id === action.id)
-            return [...state,slice(0, idx), ...state.slice(idx + 1)]
+            return [...state.slice(0, idx), ...state.slice(idx + 1)]
 
         default:
             return state
