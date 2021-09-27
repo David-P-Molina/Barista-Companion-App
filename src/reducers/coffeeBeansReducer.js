fadb2e08d87a096a4b186da47e215e444f84a5cb
David Moina <davemolina777@gmail.com>
2021-09-27 17:34:03 -0500

    Add delete functionality to reducer

--- src/reducers/coffeeBeansReducer.js
+++ src/reducers/coffeeBeansReducer.js
@@ -1,7 +1,13 @@
 function coffeeBeansReducer(state = [], action) {
+    let idx
     switch (action.type) {
         case 'ADD_COFFEE_BEAN':
             return [...state, action.book]
+            
+        case 'DELETE_COFFEE_BEAN':
+            idx = state.findIndex((bean) => bean.id === action.id)
+            return [...state,slice(0, idx), ...state.slice(idx + 1)]
+
         default:
             return state
     }
