b76de357a297c28e14a5499a251e492f22e57e3c
David Moina <davemolina777@gmail.com>
2021-09-27 17:22:57 -0500

    Add basic user reducer commented out

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,11 +3,13 @@ import brewMethodsReducer from './brewMethodsReducer'
 import coffeeBeansReducer from './coffeeBeansReducer'
 import recipesReducer from './recipesReducer'
 import roastersReducer from './roastersReducer'
+// import userReducer from './userReducer'
 
 const rootReducer = combineReducers({
     brewMethods: brewMethodsReducer,
     coffeeBeans: coffeeBeansReducer,
     recipes: recipesReducer,
+    // user: userReducer,
     roasters: roastersReducer
 })
 
