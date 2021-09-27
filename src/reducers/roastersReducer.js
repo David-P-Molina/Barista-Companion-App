5f92b7344df4ae6551e309b86abbf017aad832bb
David Moina <davemolina777@gmail.com>
2021-09-27 17:41:04 -0500

    Import uuid and add basic addCoffeBean action

--- src/reducers/roastersReducer.js
+++ src/reducers/roastersReducer.js
@@ -1,3 +1,5 @@
+import uuid from 'uuid'
+
 function roastersReducer(state = [], action) {
     let idx
     switch (action.type) {
@@ -8,6 +10,16 @@ function roastersReducer(state = [], action) {
             idx = state.findIndex((roaster) => roaster.id === action.id)
             return [...state,slice(0, idx), ...state.slice(idx + 1)]
 
+        case 'ADD_COFFEE_BEAN':
+            let existingRoaster = state.filter(
+                (roaster) => roaster.roasterName === action.coffeeBean.roasterName
+            )
+            if (existingRoaster.length > 0) {
+                return state 
+            } else {
+                return [...state, {roasterName: action.coffeeBean.roasterName, id: uuid() }]
+            }
+        
         default:
             return state
     }
