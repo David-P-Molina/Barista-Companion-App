dff755725b4fc128c08adbcc6c39bafc82e0b682
David Moina <davemolina777@gmail.com>
2021-09-28 16:15:22 -0500

    Create basic Roaster form

---
+++ src/components/RoasterForm.js
@@ -0,0 +1,16 @@
+import React from 'react' 
+
+class RoasterForm extends React.Component {
+
+    render() {
+        return(
+            <div className='roaster-form'>
+                <form>
+                    
+                </form>
+            </div>
+        )
+    }
+}
+
+export default RoasterForm
