31ef64360f534918cc2e0b586594dea96321d0eb
David Moina <davemolina777@gmail.com>
2021-09-28 10:10:29 -0500

    Fix and format state and brew methods state

--- src/reducers/brewMethodsReducer.js
+++ src/reducers/brewMethodsReducer.js
@@ -1,12 +1,16 @@
-function brewMethodsReducer(state = [], action) {
+function brewMethodsReducer(
+    state = {
+        brewMethods: [],
+    }, action
+    ) {
     let idx
     switch (action.type) {
         case 'ADD_BREW_METHOD':
-            return [...state, action.brew]
+            return { brewMethods: [...state.brewMethods, action.payload]}
             
         case 'DELETE_BREW_METHOD':
             idx = state.findIndex((brew) => brew.id === action.id)
-            return [...state.slice(0, idx), ...state.slice(idx + 1)]
+            return [...state.slice(0, idx), ...state.slice(idx + 1)]//fix to reflect portion of state
 
         default:
             return state
