507daeff4c8e916753fdad2e653144443f69f543
David Moina <davemolina777@gmail.com>
2021-09-29 11:37:53 -0500

    Abstract recipe redux from form to container

--- src/containers/RecipesContainer.js
+++ src/containers/RecipesContainer.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import RecipeForm from '../components/RecipeForm'
+import RecipesList from '../components/RecipesList'
 
 class RecipesContainer extends Component {
-    
     render() {
         return (
             <div className='recipes-container'>
-                
+                <RecipeForm addRecipe={this.props.addRecipe} />
+                <RecipesList recipes={this.props.recipes} />
             </div>
         )
     }
 }
+const mapStateToProps = ({recipes}) => ({recipes})
 const mapDispatchToProps = () => {
     return {
         addRecipe: (formData) => dispatchEvent({ type: 'ADD_RECIPE', payload: formData })
     }
 }
-export default connect(null, mapDispatchToProps)(RecipesContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(RecipesContainer)
