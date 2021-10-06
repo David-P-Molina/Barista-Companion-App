57c8c333677fdbc16e22aaeed309ba704c4ca339
David Moina <davemolina777@gmail.com>
2021-10-05 20:05:19 -0500

    Add year founded and anchor tag

--- src/components/Roaster.js
+++ src/components/Roaster.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Roaster = (props) => {
     return (
         <div className='roaster-card'>
-            <h1>{props.name}</h1>
+            <a href={props.url}><h1>{props.name}</h1></a>
+            <h3>{props.yearFounded}</h3>
             <p>{props.description}</p>
         </div>
     )
