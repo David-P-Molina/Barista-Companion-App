7b7a29d70804e0ca6c4495c2635912c30f00cadf
David Moina <davemolina777@gmail.com>
2021-10-05 20:12:06 -0500

    Add basic recipe reducer

---
+++ src/reducers/recipesReducer.js
@@ -0,0 +1,13 @@
+function recipesReducer(
+    state = {
+        recipes: [],
+    }, action
+    ) {
+    switch (action.type) {
+        case 'ADD_RECIPE':
+            return
+        default:
+            return state
+}
+
+export default recipesReducer
