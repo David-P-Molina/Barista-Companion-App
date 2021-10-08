6b459f1e25eec0b5bbacecb990e3a63d4470bcee
David Moina <davemolina777@gmail.com>
2021-10-08 14:24:36 -0500

    Create basic form action and config object

---
+++ src/actions/CoffeeBeanAction.js
@@ -0,0 +1,13 @@
+export const sendCoffeeBeanDataAction = (data) => {
+    return (passingDispatchFn) => {
+        const configObj = {
+            headers: {
+                "content-type": "application/json",
+                "accepts": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify(data)
+        }
+    
+    }
+}
