e81c0dd08071f162786b1757be88c0a901c048e4
David Moina <davemolina777@gmail.com>
2021-10-09 19:19:05 -0500

    Add basic recipe action with structure of call

---
+++ src/actions/RecipeAction.js
@@ -0,0 +1,18 @@
+export const sendRecipeDataAction = (data) => {
+    return (passingDispatchFn) => {
+        const configObj = {
+            headers: {
+                "content-type": "application/json",
+                "accepts": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify(data)
+        }
+
+        fetch("database-url", configObj)
+        .then((response) => response.json())
+        .then((data)=> {
+            
+        })
+    }
+}
