34f74bee32e785ca0a3f397cf7155ae62c354be4
David Moina <davemolina777@gmail.com>
2021-10-11 19:24:40 -0500

    Create config obj

--- src/actions/RoasterAction.js
+++ src/actions/RoasterAction.js
@@ -1,11 +1,18 @@
 export const sendRoasterDataAction = (data) => {
     return (passingDispatchFn) => {
-        
+        const configObj = {
+            headers: {
+                "content-type": "application/json",
+                "accepts": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify(data)
+        }
 
-fetch("database-url", configObj)
-.then((response) => response.json())
-.then((data) => {
-    
-})
+        fetch("database-url", configObj)
+        .then((response) => response.json())
+        .then((data) => {
+
+        })
     }
 }
