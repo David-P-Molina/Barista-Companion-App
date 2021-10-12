d57f772211133b5ba26c66de9946020919598931
David Moina <davemolina777@gmail.com>
2021-10-11 19:18:26 -0500

    Add redux connect and import action

--- src/components/RoasterForm.js
+++ src/components/RoasterForm.js
@@ -1,4 +1,6 @@
 import React from 'react' 
+import { connect } from 'react-redux'
+import { sendRoasterDataAction } from '../actions/RoasterAction'
 
 class RoasterForm extends React.Component {
     state = {
