4088786f9169286fcae36ff1be699f75d529557b
David Moina <davemolina777@gmail.com>
2021-10-14 11:45:40 -0500

    create ul that iterates over a list of equipment and display

--- src/components/BrewMethod.js
+++ src/components/BrewMethod.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
 const BrewMethod = ({props}) => {
+    const equipmentArray =  props.equipment.split(", ")
+    const listOfEquipment = equipmentArray.map((equipment) => <li>{equipment}</li>)
+    console.log(equipmentArray)
         return (
             <div className='brew-method'>
                 <h1>{props.id} - {props.name}</h1>
                 <p>{props.description}</p>
-                <h4>{props.equipment}</h4>
-                {/* Iterate over a list of equipment in a ul or import an equipmentlist container */}
+                <h4>What You Need</h4>
+                <ul>
+                    {listOfEquipment}
+                </ul>
             </div>
         )
 }
