8cacbd3d0b3439d98531535e34502a1122556d3a
David Moina <davemolina777@gmail.com>
2021-10-14 13:16:23 -0500

    Add action and mapdispatchToprops

--- src/containers/CoffeeBeansContainer.js
+++ src/containers/CoffeeBeansContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import CoffeeBeanForm from '../components/CoffeeBeanForm'
 import CoffeeBeanList from '../components/CoffeeBeansList'
+import { fetchCoffeeBeans } from '../actions/CoffeeBeanAction'
 
 class CoffeeBeansContainer extends Component {
     constructor() {
@@ -11,9 +12,7 @@ class CoffeeBeansContainer extends Component {
         }
     }
     componentDidMount() {
-        fetch('ruby-api-url')
-        .then((response) => response.json())
-        .then((data) => this.setState({ coffeeBeans: data.coffeeBeans}))
+        this.props.fetchCoffeeBeans()
     }
     render() {
         return (
@@ -27,6 +26,7 @@ class CoffeeBeansContainer extends Component {
 const mapStateToProps = ({coffeeBeans}) => ({coffeeBeans})
 const mapDispatchToProps = (dispatch) => {
     return {
+        fetchCoffeeBeans: () => dispatch(fetchCoffeeBeans()) ,
         addCoffeeBean: (formData) => dispatch({ type: "ADD_COFFEE_BEAN", payload: formData })
     }
 }
