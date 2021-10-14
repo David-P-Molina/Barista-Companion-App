6ef5703aba0adb658af8359b1d7e3dca915b544f
David Moina <davemolina777@gmail.com>
2021-10-14 13:06:08 -0500

    Create fetch call and add error handling

--- src/actions/CoffeeBeanAction.js
+++ src/actions/CoffeeBeanAction.js
@@ -15,3 +15,23 @@ export const sendCoffeeBeanDataAction = (data) => {
         })
     }
 }
+
+export function fetchCoffeeBeans() {
+    return (dispatch) => {
+        dispatch({type: 'START_LOADING_COFFEE_BEANS'})
+        fetch('http://localhost:3000/coffee_beans')
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+                .then((coffeeBeans) => {
+                    let beansArray = []
+                    coffeeBeans.data.forEach((bean) => beansArray.push(bean.attributes))
+                    dispatch({type: 'FETCH_COFFEE_BEANS', coffeeBeans: beansArray})
+                })
+            } else {
+                return response.json()
+                .catch((errors) => console.log(errors))
+            }
+        })
+    }
+}
