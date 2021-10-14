6716ef603f9832cb892f1b3e5057d497a45641d9
David Moina <davemolina777@gmail.com>
2021-10-14 14:42:29 -0500

    Import action and add mapDispatchToProps

--- src/containers/RoastersContainer.js
+++ src/containers/RoastersContainer.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import RoastersList from '../components/RoastersList'
 import RoasterForm from '../components/RoasterForm'
+import { fetchRoasters } from '../actions/RoasterAction'
 
 class RoastersContainer extends Component {
     state = {
         roaster: [],
     }
     componentDidMount() {
-        
+        this.props.fetchRoasters()
     }
     render() {
         return (
             <div className='brew-method-container'>
-                <RoasterForm addRoaster={this.props.add}/>
+                {/* <RoasterForm addRoaster={this.props.addRoaster}/> */}
                 <RoastersList roasters={this.props.roasters} />
             </div>)
     }
@@ -21,6 +22,7 @@ class RoastersContainer extends Component {
 const mapStateToProps =({roasters}) => ({roasters})
 const mapDispatchToProps = (dispatch) => {
     return {
+        fetchRoasters: () => dispatch(fetchRoasters()),
         addRoaster: (formData) => dispatch({ type: "ADD_ROASTER", payload: formData })
     }
 }
