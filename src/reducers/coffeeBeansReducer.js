0a9a0b968deb95b7c64095b332d66c6b330e77de
David Moina <davemolina777@gmail.com>
2021-10-14 13:10:19 -0500

    Format file and add loading and fetch case statements

--- src/reducers/coffeeBeansReducer.js
+++ src/reducers/coffeeBeansReducer.js
@@ -1,16 +1,38 @@
 function coffeeBeansReducer(
     state = {
         coffeeBeans: [],
+        loadingCoffeeBeans: false
     }, action
     ) {
     let idx
     switch (action.type) {
+        case 'START_LOADING_COFFEE_BEANS':
+            return {
+                ...state,
+                loadingCoffeeBeans: true,
+            
+            }
+
+        case 'FETCH_COFFEE_BEANS':
+            return {
+                ...state, 
+                coffeeBeans: action.coffeeBeans, 
+                loadingCoffeeBeans: false
+            }
+        
         case 'ADD_COFFEE_BEAN':
-            return { coffeeBeans: [...state.coffeeBeans, action.payload]}
+            return { 
+                ...state,
+                coffeeBeans: [...state.coffeeBeans, action.payload]
+            }
 
         case 'DELETE_COFFEE_BEAN':
             idx = state.coffeeBeans.findIndex((bean) => bean.id === action.id)
-            return { coffeeBeans: [...state.coffeeBeans.slice(0, idx), ...state.coffeeBeans.slice(idx + 1)]}
+            return { 
+                ...state,
+                coffeeBeans: [...state.coffeeBeans.slice(0, idx), 
+                ...state.coffeeBeans.slice(idx + 1)]
+            }
 
         default:
             return state
