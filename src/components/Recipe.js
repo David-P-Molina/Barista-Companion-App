a9aa3f1c4621461febb07dfe7ea3c3459c4c0cd6
David Moina <davemolina777@gmail.com>
2021-10-17 20:33:31 -0500

    Add additional display information

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -5,12 +5,14 @@ const Recipe = (props) => {
     return (
         <div className='recipe'>
             <h1>{props.name}</h1>
+            <span>{props.brew_method_name}</span>
+            Bean: {props.coffee_bean_name} by {props.roaster_name}
             <ul>
                 <li>Ingredients</li>
             </ul>
             {/* <Timer /> */}
         </div>
     )
-}
+ }
 
 export default Recipe
