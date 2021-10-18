6e9dab7b98f18f0d2cfae0f95d3a039a795fc735
David Moina <davemolina777@gmail.com>
2021-10-18 17:38:44 -0500

    Add required rendering information

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -6,11 +6,13 @@ const Recipe = (props) => {
         <div className='recipe'>
             <h1>{props.name}</h1>
             <span>{props.brew_method_name}</span>
-            Bean: {props.coffee_bean_name} by {props.roaster_name}
-            <ul>
-                <li>Ingredients</li>
-            </ul>
+            Bean: {props.coffee_bean_name} by {props.roaster_name} Roast Date: {props.roast_date}
+            Grind Size: {props.grind} Coffee Weight: {props.coffee_in_grams} grams
+            <h4>Water: {props.water_in_grams} grams</h4><h4>{props.temperature} °F</h4>
+            <h4>Filter </h4> 
+            <h4>Brew Time: {props.time}, Bloom Time: {props.bloom_time}</h4>
             {/* <Timer /> */}
+            Notes: {props.notes}
         </div>
     )
  }
