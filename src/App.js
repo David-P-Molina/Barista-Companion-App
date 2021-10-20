90b4e5264151efa3d328df554690df3b3d1dab07
David Moina <davemolina777@gmail.com>
2021-10-20 12:41:45 -0500

    Convert app.js to class Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
 import BrewMethodsContainer from './containers/BrewMethodsContainer';
 import CoffeeBeansContainer from './containers/CoffeeBeansContainer'
 import RoastersContainer from './containers/RoastersContainer'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <NavBar /> */}
-      </header>
-      <Router>
-        <>
-          <Route exact path='/'>
-            <BrewMethodsContainer />
-          </Route>
-          {/* <Route exact path='/coffee-beans' component={CoffeeBeansContainer} />
-          <Route exact path='/roasters' component={RoastersContainer} /> */}
-        </>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <NavBar /> */}
+        </header>
+        <Router>
+          <>
+            <Route exact path='/'>
+              <BrewMethodsContainer />
+            </Route>
+            {/* <Route exact path='/coffee-beans' component={CoffeeBeansContainer} />
+            <Route exact path='/roasters' component={RoastersContainer} /> */}
+          </>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
