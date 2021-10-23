4c8b1c5fcc50f9bb95d6027bc7eb8f1c8abeb906
David Moina <davemolina777@gmail.com>
2021-10-23 09:32:57 -0500

    Calculate ratio

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const Recipe = ({props}) => {
+    const ratio = (props.water_in_grams / props.coffee_in_grams).toFixed(2) 
     return (
         <div className='recipe'>
             <h1>{props.name} Brewed on {props.date_attempted}</h1> 
             <span>{props.brew_method_name}</span>
             Bean: {props.coffee_bean_name} by {props.roaster_name} Roast Date: {props.roast_date}
             Grind Size: {props.grind} Coffee Weight: {props.coffee_in_grams} grams- Grind Consistency: {props.grind}
-            <h4>Water: {props.water_in_grams} grams</h4><h4>{props.temperature} °F</h4>
+            <h4>Water: {props.water_in_grams} grams</h4><h4>{props.temperature} °F</h4> 1:{ratio} Water to Coffee Ratio
             <h4>Filter {props.filter}</h4> 
             <h4>Brew Time: {props.time}, Bloom Time: {props.bloom_time}</h4>
-            {/* <Timer /> */}
             Notes: {props.notes}
         </div>
     )
