c7ebd4f6dc7708bcbe9aa339ea048afcb71d14ee
David Moina <davemolina777@gmail.com>
2021-10-29 18:33:21 -0500

    Add bootstrap design

--- src/components/RoasterForm.js
+++ src/components/RoasterForm.js
@@ -1,4 +1,5 @@
 import React from 'react' 
+import { Container, Col, Row, Form, Button } from "react-bootstrap";
 import Error from './Error'
 
 class RoasterForm extends React.Component {
@@ -34,44 +35,75 @@ class RoasterForm extends React.Component {
         let currentYear = new Date().getFullYear()
         return(      
             <div>
-                <h1>Add New Roaster</h1>
-                <div className='errors'>
-                    <Error errors={this.props.errors}/>
-                </div>
-            <form className='roaster-form' onSubmit={this.handleOnSubmit} >
-                <label htmlFor='roaster-brand-name'>Roaster Brand: </label><br />
-                <input
-                    required type='text'
-                    name='name'
-                    placeholder='Brand Name'
-                    value={this.state.name}
-                    onChange={this.handleOnChange}/><br />
-                <label htmlFor='roaster-description'>Description: </label><br />
-                <input
-                    required type='text'
-                    name='description'
-                    placeholder='History of Brand'
-                    value={this.state.description}
-                    onChange={this.handleOnChange}/><br />
-                <label htmlFor='roaster-year-founded'>Year Established/Founded: </label><br />
-                <input
-                    type='number'
-                    name='yearFounded'
-                    min='1870'
-                    max={currentYear}
-                    value={this.state.yearFounded}
-                    onChange={this.handleOnChange}/><br />
-                <label htmlFor='roaster-website'>Website: </label><br />
-                <input
-                    type='url'
-                    name='website'
-                    placeholder='www.roaster-website-url.com'
-                    value={this.state.website}
-                    onChange={this.handleOnChange}/><br />
-                <button type='submit'>Add Roaster</button>
-            </form>
-            <br />
-            <button onClick={() => this.props.return()}>Go Back To Roasters</button>
+                <Container>
+                    <Row>
+                        <Col md={{ span: 8, offset: 2}}>
+                        <div className='form-background'>
+                            <h1>Roaster Form</h1>
+                            <p>With the Third Wave of coffee there are now hundreds of amazing coffee roaster brands to try! Each one offering distinct beans sourced from all over the world. Have you found one that you like?  Fill out this form along with some of their coffee beans so other people can learn more about your most recent roaster obsession!</p>
+                            <div className='errors'>
+                                <Error errors={this.props.errors}/>
+                            </div>
+                        <Form className='roaster-form' onSubmit={this.handleOnSubmit} >
+                            <Form.Group classname='mb-3' controlId='formRoasterName'>    
+                                <Form.Label htmlFor='roaster-brand-name'>Name: </Form.Label>
+                                <Form.Control
+                                    required type='text'
+                                    name='name'
+                                    placeholder='Brand Name'
+                                    value={this.state.name}
+                                    onChange={this.handleOnChange}/>
+                                <Form.Text className='text-muted'>
+                                    *required
+                                </Form.Text>
+                            </Form.Group>
+                            <Form.Group classname='mb-3' controlId='formRoasterHistory'>        
+                                <Form.Label htmlFor='roaster-description'>History: </Form.Label><br />
+                                <Form.Control
+                                    required type='text'
+                                    name='description'
+                                    placeholder='History of Brand'
+                                    value={this.state.description}
+                                    onChange={this.handleOnChange}/>
+                                <Form.Text className='text-muted'>
+                                    *required
+                                </Form.Text>
+                            </Form.Group>
+                            <Row>
+                                <Col>
+                                    <Form.Group classname='mb-3' controlId='formYearFounded'>    
+                                        <Form.Label htmlFor='roaster-year-founded'>Year Established/Founded: </Form.Label><br />
+                                        <Form.Control
+                                            type='number'
+                                            name='yearFounded'
+                                            min='1870'
+                                            max={currentYear}
+                                            value={this.state.yearFounded}
+                                            onChange={this.handleOnChange}/>
+                                        <Form.Text className='text-muted'>
+                                                    *1870 to {currentYear}
+                                        </Form.Text>
+                                    </Form.Group>
+                                </Col>
+                                <Col>
+                                    <Form.Group classname='mb-3' controlId='formWebsite'>    
+                                        <Form.Label htmlFor='roaster-website'>Website: </Form.Label><br />
+                                        <Form.Control
+                                            type='url'
+                                            name='website'
+                                            placeholder='www.roaster-website-url.com'
+                                            value={this.state.website}
+                                            onChange={this.handleOnChange}/>
+                                    </Form.Group>
+                                </Col>
+                            </Row><br />
+                            <Button variant='success' size='lg' type='submit'>Add Roaster</Button>
+                        </Form><br />
+                        <Button variant='secondary' size='sm' onClick={() => this.props.return()}>Go Back To Roasters</Button>
+                        </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
