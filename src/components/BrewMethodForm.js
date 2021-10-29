24c43f225ade5d27f3ff516f8955229cfdecd875
David Moina <davemolina777@gmail.com>
2021-10-29 17:14:26 -0500

    Add react bootstrap form & accessebility info

--- src/components/BrewMethodForm.js
+++ src/components/BrewMethodForm.js
@@ -47,43 +47,57 @@ class BrewMethodForm extends Component {
                 <Container>
                     <Row>
                         <Col md={{ span: 8, offset: 2 }}>
-                        <div className='form-background'>
+                            <div className='form-background'>
                                 <h1>Brew Method Form</h1>
                                 <p>Do you have a unique way of brewing coffee? Perhaps you brew using six filters, a sandwich bag, and a shoe! Fill out this form and add you'll have access to that brewing method in our recipes section!</p>
                                 <span><i>*When filling out the equipment section please separate each item with a comma to display correctly.</i></span>
                                 <div className='errors'>
-                    <Error errors={this.props.errors}/>
-                </div>
-            <form className='brew-method-form' onSubmit={this.handleOnSubmit}>
-                <label htmlFor='brew-method-name'>Brew Method: </label><br />
-                <input 
-                    required type='text' 
-                    name='name'
-                    placeholder='Brewing Method'
-                    value={this.state.name} 
-                    onChange={this.handleOnChange} /><br />
-                <label htmlFor='brew-method-description'>Description: </label><br />
-                <input 
-                    required type='text' 
-                    name='description'
-                    placeholder='Description'
-                    value={this.state.description} 
-                    onChange={this.handleOnChange} /><br />
-                <label htmlFor='brew-method-equipment'>Equipment: (Please Separate by ",")</label><br />
-                <input 
-                    required type='text' 
-                    name='equipment'
-                    placeholder='List of equipment needed'
-                    value={this.state.equipment} 
-                    onChange={this.handleOnChange} /><br /><br />
-                <Button variant='success' size='lg' type='submit'>Add Brew Method</Button>
-            </form>
-            <br/>
-            <Button variant='secondary' size='sm' onClick={() => this.props.return()}>Go Back To Brew Methods</Button>
-            </div>
-            </Col>
-            </Row>
-            </Container>
+                                    <Error errors={this.props.errors}/>
+                            </div>
+                            <Form className='brew-method-form' onSubmit={this.handleOnSubmit}>
+                                    <Form.Group className="mb-3" controlId='formBrewMethodName'>
+                                        <Form.Label htmlFor='brew-method-name'>Name: </Form.Label><br />
+                                        <Form.Control 
+                                            required type='text' 
+                                            name='name'
+                                            placeholder='Brewing Method'
+                                            value={this.state.name} 
+                                            onChange={this.handleOnChange} />
+                                        <Form.Text className='text-muted'>
+                                            *required
+                                        </Form.Text>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId='formBrewMethodDescription'>
+                                        <Form.Label htmlFor='brew-method-description'>Description: </Form.Label><br />
+                                        <Form.Control
+                                            required type='text' 
+                                            name='description'
+                                            placeholder='Description'
+                                            value={this.state.description} 
+                                            onChange={this.handleOnChange} />
+                                        <Form.Text className='text-muted'>
+                                            *required
+                                        </Form.Text>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId='formBrewMethodEquipment'>
+                                        <Form.Label htmlFor='brew-method-equipment'>Equipment: (Please Separate by ",")</Form.Label><br />
+                                        <Form.Control 
+                                            required type='text' 
+                                            name='equipment'
+                                            placeholder='List of equipment needed'
+                                            value={this.state.equipment} 
+                                            onChange={this.handleOnChange} />
+                                        <Form.Text className='text-muted'>
+                                            *required
+                                        </Form.Text>
+                                    </Form.Group>
+                                    <Button variant='success' size='lg' type='submit'>Add Brew Method</Button>
+                            </Form><br/>
+                            <Button variant='secondary' size='sm' onClick={() => this.props.return()}>Go Back To Brew Methods</Button>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
