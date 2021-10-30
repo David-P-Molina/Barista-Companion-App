d5a698ca42d5a881b81edd16bc9dc30bef1160c4
David Moina <davemolina777@gmail.com>
2021-10-30 10:00:09 -0500

    Update Roaster form

--- src/components/RoasterForm.js
+++ src/components/RoasterForm.js
@@ -37,40 +37,49 @@ class RoasterForm extends React.Component {
             <div>
                 <Container>
                     <Row>
-                        <Col md={{ span: 8, offset: 2}}>
+                        <Col md={{ span: 10, offset: 1}}>
                         <div className='form-background'>
                             <h1>Roaster Form</h1>
-                            <p>With the Third Wave of coffee there are now hundreds of amazing coffee roaster brands to try! Each one offering distinct beans sourced from all over the world. Have you found one that you like?  Fill out this form along with some of their coffee beans so other people can learn more about your most recent roaster obsession!</p>
+                            <p>With the Third Wave of coffee, there are now hundreds of amazing coffee roaster brands to try! Each one offering distinct beans sourced from all over the world. Have you found one that you like?  Fill out this form along with some of their coffee beans so other people can learn more about your most recent roaster obsession!</p>
                             <div className='errors'>
                                 <Error errors={this.props.errors}/>
                             </div>
                         <Form className='roaster-form' onSubmit={this.handleOnSubmit} >
-                            <Form.Group className='mb-3'>    
-                                <Form.Label htmlFor='roaster-brand-name'>Name: </Form.Label>
-                                <Form.Control
-                                    required type='text'
-                                    name='name'
-                                    placeholder='Brand Name'
-                                    value={this.state.name}
-                                    onChange={this.handleOnChange}/>
-                                <Form.Text className='text-muted'>
-                                    *required
-                                </Form.Text>
-                            </Form.Group>
-                            <Form.Group className='mb-3'>        
-                                <Form.Label htmlFor='roaster-description'>History: </Form.Label><br />
-                                <Form.Control
-                                    required type='text'
-                                    name='description'
-                                    placeholder='History of Brand'
-                                    value={this.state.description}
-                                    onChange={this.handleOnChange}/>
-                                <Form.Text className='text-muted'>
-                                    *required
-                                </Form.Text>
-                            </Form.Group>
                             <Row>
                                 <Col>
+                                    <Form.Group className='mb-3'>    
+                                        <Form.Label htmlFor='roaster-brand-name'>Name: </Form.Label>
+                                        <Form.Control
+                                            required type='text'
+                                            name='name'
+                                            placeholder='Ruby, Counter Culture, Heart, MadCap, etc.'
+                                            value={this.state.name}
+                                            onChange={this.handleOnChange}/>
+                                        <Form.Text className='text-muted'>
+                                            *required
+                                        </Form.Text>
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Form.Group className='mb-3'>        
+                                        <Form.Label htmlFor='roaster-description'>History: </Form.Label><br />
+                                        <Form.Control
+                                            required as='textarea'
+                                            name='description'
+                                            placeholder='Founded by, started at a cafe located at, etc.'
+                                            value={this.state.description}
+                                            onChange={this.handleOnChange}
+                                            row={3}/>
+                                        <Form.Text className='text-muted'>
+                                            *required
+                                        </Form.Text>
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs='auto'>
                                     <Form.Group className='mb-3' >    
                                         <Form.Label htmlFor='roaster-year-founded'>Year Established/Founded: </Form.Label><br />
                                         <Form.Control
@@ -78,6 +87,7 @@ class RoasterForm extends React.Component {
                                             name='yearFounded'
                                             min='1870'
                                             max={currentYear}
+                                            placeholder='2010'
                                             value={this.state.yearFounded}
                                             onChange={this.handleOnChange}/>
                                         <Form.Text className='text-muted'>
@@ -97,9 +107,13 @@ class RoasterForm extends React.Component {
                                     </Form.Group>
                                 </Col>
                             </Row><br />
-                            <Button variant='success' size='lg' type='submit'>Add Roaster</Button>
+                            <Row>
+                                <Button mx='auto' variant='success' size='lg' type='submit'>Add Roaster</Button>
+                            </Row>
                         </Form><br />
-                        <Button variant='secondary' size='sm' onClick={() => this.props.return()}>Go Back To Roasters</Button>
+                        <Row>
+                            <Button mx='auto' variant='secondary' size='sm' onClick={() => this.props.return()}>Roasters List</Button>
+                        </Row>
                         </div>
                         </Col>
                     </Row>
