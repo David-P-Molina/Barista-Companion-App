9136864354140ea2923ca7824c0e68c010ea025c
David Moina <davemolina777@gmail.com>
2021-10-30 22:49:25 -0500

    Remove User reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -4,13 +4,11 @@ import coffeeBeansReducer from './coffeeBeansReducer'
 import recipesReducer from './recipesReducer'
 import roastersReducer from './roastersReducer'
 import errorsReducer from './errorsReducer'
-// import userReducer from './userReducer'
 
 const rootReducer = combineReducers({
     brewMethods: brewMethodsReducer,
     coffeeBeans: coffeeBeansReducer,
     recipes: recipesReducer,
-    // user: userReducer,
     roasters: roastersReducer,
     errors: errorsReducer
 })
