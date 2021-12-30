6fc53fd9dd8e0357bce2b3eca9cca0513ac60266
David Moina <davemolina777@gmail.com>
2021-12-29 20:44:00 -0600

    Create basic comment form

---
+++ src/components/CommentForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Form } from 'react-bootstrap'
+
+class CommentForm extends Component {
+    render() {
+        return (
+            <Form>
+                
+            </Form>
+        )
+    }
+}
+
+export default CommentForm
